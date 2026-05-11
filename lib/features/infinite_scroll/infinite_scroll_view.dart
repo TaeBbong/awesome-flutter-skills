af355b1d0a152580cd1561515267b57490f7d48a
TaeBbong <mok05289@korea.ac.kr>
2026-05-11 21:06:53 +0900

    feat: enhance infinite_scroll with hasMore flag

--- lib/features/infinite_scroll/infinite_scroll_view.dart
+++ lib/features/infinite_scroll/infinite_scroll_view.dart
@@ -11,21 +11,24 @@ class InfiniteScrollView extends StatefulWidget {
 class _InfiniteScrollViewState extends State<InfiniteScrollView> {
   final ProfileRepository _profileRepository = ProfileRepository();
   final ScrollController _scrollController = ScrollController();
+
+  final List<Profile> _profiles = [];
   int _currentPage = 0;
   bool _isLoading = false;
-  List<Profile> _profiles = [];
+  bool _hasMore = true;
 
   void _fetchNextPage() async {
-    if (_isLoading) return;
+    if (_isLoading || !_hasMore) return;
     setState(() {
       _isLoading = true;
     });
-    List<Profile> response = await _profileRepository.fetchProfiles(
+    ProfileFetchResult response = await _profileRepository.fetchProfiles(
       page: _currentPage,
     );
     setState(() {
-      _profiles.addAll(response);
+      _profiles.addAll(response.items);
       _isLoading = false;
+      if (_profiles.length >= response.total) _hasMore = false;
       _currentPage += 1;
     });
   }
@@ -54,14 +57,20 @@ class _InfiniteScrollViewState extends State<InfiniteScrollView> {
       appBar: AppBar(title: Text('Infinite Scroll')),
       body: ListView.builder(
         controller: _scrollController,
-        itemCount: _profiles.length + (_isLoading ? 1 : 0),
+        itemCount: _profiles.length + (_isLoading || !_hasMore ? 1 : 0),
         itemBuilder: (context, idx) {
-          if (idx >= _profiles.length) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return ListTile(
-            title: Text('${_profiles[idx].id} : ${_profiles[idx].username}'),
-          );
+          return idx >= _profiles.length
+              ? _isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : const Padding(
+                        padding: EdgeInsets.all(14),
+                        child: Center(child: Text('No more data to fetch')),
+                      )
+              : ListTile(
+                  title: Text(
+                    '${_profiles[idx].id} : ${_profiles[idx].username}',
+                  ),
+                );
         },
       ),
     );
--- lib/shared/data/profile_repository.dart
+++ lib/shared/data/profile_repository.dart
@@ -4,14 +4,25 @@ import 'package:http/http.dart' as http;
 
 import '../../shared/domain/models/profile.dart';
 
+class ProfileFetchResult {
+  final List<Profile> items;
+  final int total;
+
+  ProfileFetchResult({required this.items, required this.total});
+}
+
 class ProfileRepository {
-  Future<List<Profile>> fetchProfiles({required int page}) async {
+  Future<ProfileFetchResult> fetchProfiles({required int page}) async {
     final String url = 'https://dummyjson.com/users?limit=20&skip=${page * 20}';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       List<dynamic> raw = json.decode(response.body)['users'];
-      return raw.map((profile) => Profile.fromJson(profile)).toList();
+      int total = json.decode(response.body)['total'];
+      return ProfileFetchResult(
+        items: raw.map((profile) => Profile.fromJson(profile)).toList(),
+        total: total,
+      );
     } else {
       throw Exception('Failed to load profiles from server');
     }
